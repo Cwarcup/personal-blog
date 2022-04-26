4d117cfa015227be9652ccf409f88a1a8816c5ce
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-04-26 10:02:19 -0700

    homepage changes

--- pages/delete.js
+++ pages/delete.js
@@ -4,11 +4,13 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
+import { RoughNotation } from 'react-rough-notation'
 import Image from '@/components/Image'
+import ShortcutHome from '@/components/ShortcutHome'
 
 import NewsletterForm from '@/components/NewsletterForm'
 
-const MAX_DISPLAY = 8
+const MAX_DISPLAY = 3
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
@@ -21,21 +23,61 @@ export default function Home({ posts }) {
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 md:space-y-5">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <div className="flex items-center justify-between">
             <div className="">
-              <h1 className="pb-4 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-                Hi, I'm Curtis Warcup
+              <h1 className="mb-2 text-2xl font-extrabold leading-11 tracking-tight text-slate-900 dark:text-slate-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+                I'm <span className="text-primary-color dark:text-primary-color-dark">Einar</span>,
+                a curious software developer who's trying to get a bit better every day
               </h1>
             </div>
+            <div>
+              <Image
+                src="/static/images/einar-avatar-new.jpg"
+                alt="avatar"
+                width="384px"
+                height="384px"
+                className=" [clip-path:polygon(0%_0%,100%_0%,50%_100%,0%_50%)]"
+              />
+            </div>
           </div>
-          <p className=" prose pb-4 text-lg text-gray-600 dark:text-gray-400">
-            I'm a self taught developer looking to improve my coding abilities everyday. Here is a
-            look into my personal notes, projects and passions.
+          <p className="text-lg leading-7 text-slate-600 dark:text-slate-300">
+            This is my place for{' '}
+            <RoughNotation
+              type="underline"
+              show={true}
+              color="#fff176"
+              animationDelay={800}
+              animationDuration={1200}
+            >
+              thoughts,{' '}
+            </RoughNotation>
+            <RoughNotation
+              type="underline"
+              show={true}
+              color="#ADD8E6"
+              animationDelay={1400}
+              animationDuration={1200}
+            >
+              reflections,{' '}
+            </RoughNotation>
+            &{' '}
+            <RoughNotation
+              type="underline"
+              show={true}
+              color="#FF0000"
+              animationDelay={1700}
+              animationDuration={1200}
+            >
+              everything{' '}
+            </RoughNotation>
+            in between
           </p>
-          <h2 className="flex pb-6 text-2xl font-extrabold tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl md:text-5xl">
-            Latest
-          </h2>
+          <div className="flex w-full justify-center">
+            <div className="mt-2 justify-center">
+              <ShortcutHome />
+            </div>
+          </div>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
@@ -47,7 +89,7 @@ export default function Home({ posts }) {
                   <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
                     <dl>
                       <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                      <dd className="text-base font-medium leading-6 text-slate-600 dark:text-slate-400">
                         <time dateTime={date}>{formatDate(date)}</time>
                       </dd>
                     </dl>
@@ -57,7 +99,7 @@ export default function Home({ posts }) {
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
+                              className="text-slate-800 dark:text-slate-200"
                             >
                               {title}
                             </Link>
@@ -68,14 +110,14 @@ export default function Home({ posts }) {
                             ))}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                        <div className="prose max-w-none text-slate-600 dark:text-slate-400">
                           {summary}
                         </div>
                       </div>
                       <div className="text-base font-medium leading-6">
                         <Link
                           href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          className="text-primary-color dark:text-primary-color-dark hover:text-blue-600 dark:hover:text-yellow-300"
                           aria-label={`Read "${title}"`}
                         >
                           Read more &rarr;
@@ -93,18 +135,18 @@ export default function Home({ posts }) {
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
             href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-primary-color dark:text-primary-color-dark hover:text-blue-600 dark:hover:text-yellow-300"
             aria-label="all posts"
           >
             All Posts &rarr;
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter.provider !== '' && (
+      {/* {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
