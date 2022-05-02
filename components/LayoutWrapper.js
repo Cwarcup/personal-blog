cd7ed1dbc2ab846584aea3c6c75e814233f0312f
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-02 15:14:58 -0700

    nav

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -23,3 +23,14 @@ const LayoutWrapper = ({ children }) => {
 }
 
 export default LayoutWrapper
+
+// const LayoutWrapper = ({ children }) => {
+//   const isTop = useIsScrollTop()
+//   return (
+//     <SectionContainer>
+//       <div className=" flex h-screen flex-col justify-between">
+//         <header
+//           className={`sticky top-0 z-20 flex w-full items-center justify-between py-10 ${
+//             isTop ? 'border-none' : 'border-b border-gray-200 dark:border-gray-800'
+//           } firefox:bg-opacity-100 dark:firefox:bg-opacity-100 bg-white bg-opacity-30 backdrop-blur-lg backdrop-saturate-150 backdrop-filter dark:bg-black dark:bg-opacity-30`}
+//         ></header>
