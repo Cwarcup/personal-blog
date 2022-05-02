7bf826fa37e708a076a3589b9a1b213436cd294e
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-05-02 13:31:16 -0700

    hero spacing

--- components/Hero.js
+++ components/Hero.js
@@ -5,14 +5,14 @@ import PageTitle from './PageTitle'
 export default function Hero() {
   return (
     <div className="flex w-full flex-col">
-      <div className="space-y-2 md:space-y-5">
+      <div className="space-y-2 pb-4 md:space-y-5">
         <PageTitle>
           Hi, I'm Curtis Warcup{' '}
           <span role="img" aria-label="waving hand" className="wave">
             👋
           </span>
         </PageTitle>
-        <p className="prose max-w-none text-lg leading-7 text-gray-500 dark:text-gray-400">
+        <p className=" prose max-w-none pb-4 text-lg leading-7 text-gray-500 dark:text-gray-400">
           I'm a self taught developer looking to improve my coding abilities everyday. Here is a
           look into my personal note, projects and passions.{' '}
           <Link href={`mailto:${siteMetadata.email}`}>
