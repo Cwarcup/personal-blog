0f1753c74d3f3be2e7f9aa2f188b220e5561bf4f
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-16 13:51:43 -0700

    project card grid on small screen

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -7,16 +7,16 @@ const ProjectCard = ({ title, description, imgSrc, href }) => (
     <motion.div
       whileHover={{ scale: 1.02 }}
       whileTap={{ scale: 0.98 }}
-      className=" grid grid-cols-2 grid-rows-1 items-center gap-3 overflow-hidden rounded-lg  bg-transparent py-2"
+      className=" grid items-center gap-3 overflow-hidden rounded-lg bg-transparent py-2  md:grid-cols-2 md:grid-rows-1"
     >
       <Image
         alt={title}
         src={imgSrc}
-        className="absolute inset-0 h-full w-full rounded-lg object-cover  "
+        className="absolute inset-0 col-start-1 col-end-2 row-start-1 h-full w-full rounded-lg object-cover md:col-span-2 md:col-start-1 md:row-span-1"
         width="300px"
         height="250px"
       />
-      <div className=" row-span-1 py-4 px-2">
+      <div className="py-4 px-2 md:col-span-2 md:col-start-2 md:row-span-1">
         <h2 className="mt-2 mb-2 font-bold md:text-xl">
           <Link href={href} className="dark:text-gray-100">
             {title}
