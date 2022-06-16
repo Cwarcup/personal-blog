88899afe4c38cb5713de6b27a000dc64aadcf0d4
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-16 15:10:18 -0700

    hero text center on smaller screens

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ import PageTitle from './PageTitle'
 export default function Hero() {
   return (
     <div className="flex w-full flex-col">
-      <div className="space-y-2 pb-4 md:space-y-5">
+      <div className="space-y-2 pb-4 text-center md:space-y-5 md:text-left">
         <PageTitle>
           Hi, I'm Curtis Warcup{' '}
           <span role="img" aria-label="waving hand" className="wave">
