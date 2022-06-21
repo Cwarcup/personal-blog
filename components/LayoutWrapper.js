e0ae195aee4374c633896a60fbec1dcc0b9a68c4
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-21 14:37:02 -0700

    desktop dropdown working

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -78,20 +78,22 @@ const LayoutWrapper = ({ children }) => {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                       >
-                        <Menu.Items className="absolute right-0 mt-2 w-24 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                          <div className="px-1 py-1 ">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <button
-                                  className={`${
-                                    active ? 'text-primary-400' : 'text-gray-900'
-                                  } group text flex w-full justify-end rounded-md px-2 py-2`}
-                                >
-                                  Edit
-                                </button>
-                              )}
-                            </Menu.Item>
-                          </div>
+                        <Menu.Items className="absolute right-0 mt-2 w-24 origin-top-right divide-y divide-gray-600 rounded-md bg-cardBg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {link.links.map((item, index) => (
+                            <div key={index} className="px-1 py-1">
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <button
+                                    className={`${
+                                      active ? 'text-primary-400' : 'text-gray-100'
+                                    } group flex w-full justify-center  rounded-md px-2 py-2 font-bold`}
+                                  >
+                                    <Link href={item.href}>{item.title}</Link>
+                                  </button>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          ))}
                         </Menu.Items>
                       </Transition>
                     </Menu>
