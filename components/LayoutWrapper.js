faa2a605a75ce291c63a3e4f199c8d6718e4353c
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-06-21 14:28:01 -0700

    drop down coming together

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -78,107 +78,16 @@ const LayoutWrapper = ({ children }) => {
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                       >
-                        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Items className="absolute right-0 mt-2 w-24 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                           <div className="px-1 py-1 ">
                             <Menu.Item>
                               {({ active }) => (
                                 <button
                                   className={`${
-                                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                                    active ? 'text-primary-400' : 'text-gray-900'
+                                  } group text flex w-full justify-end rounded-md px-2 py-2`}
                                 >
-                                  {active ? (
-                                    <EditActiveIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                                  ) : (
-                                    <EditInactiveIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                                  )}
                                   Edit
-                                </button>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <button
-                                  className={`${
-                                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  {active ? (
-                                    <DuplicateActiveIcon
-                                      className="mr-2 h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <DuplicateInactiveIcon
-                                      className="mr-2 h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                  Duplicate
-                                </button>
-                              )}
-                            </Menu.Item>
-                          </div>
-                          <div className="px-1 py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <button
-                                  className={`${
-                                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  {active ? (
-                                    <ArchiveActiveIcon
-                                      className="mr-2 h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <ArchiveInactiveIcon
-                                      className="mr-2 h-5 w-5"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                  Archive
-                                </button>
-                              )}
-                            </Menu.Item>
-                            <Menu.Item>
-                              {({ active }) => (
-                                <button
-                                  className={`${
-                                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  {active ? (
-                                    <MoveActiveIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                                  ) : (
-                                    <MoveInactiveIcon className="mr-2 h-5 w-5" aria-hidden="true" />
-                                  )}
-                                  Move
-                                </button>
-                              )}
-                            </Menu.Item>
-                          </div>
-                          <div className="px-1 py-1">
-                            <Menu.Item>
-                              {({ active }) => (
-                                <button
-                                  className={`${
-                                    active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                                  } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                                >
-                                  {active ? (
-                                    <DeleteActiveIcon
-                                      className="mr-2 h-5 w-5 text-violet-400"
-                                      aria-hidden="true"
-                                    />
-                                  ) : (
-                                    <DeleteInactiveIcon
-                                      className="mr-2 h-5 w-5 text-violet-400"
-                                      aria-hidden="true"
-                                    />
-                                  )}
-                                  Delete
                                 </button>
                               )}
                             </Menu.Item>
