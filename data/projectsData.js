c7b388e4fa23f62619df3a285a63ddd8bfb2a4d4
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-10-06 12:57:22 -0500

    Add pay by phonie to project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'PayByFonie',
+    description: `PayByFonie is a play on the app PayByPhone, a parking app that allows you to pay for parking via your phone. PayByFonie is a similar app, but allows you to search a destination and view parking meter data near that location. Users can search a location using the MapBox API, view the price and time limit of parking meters near that location, and obtain the PayByPhone meter number to pay for that meter. The app was built using React, and Node.js.`,
+    imgSrc: '/static/images/projectCards/paybyfonie.jpg',
+    href: 'https://github.com/Cwarcup/free-parking-mapper',
+  },
+  {
     title: 'Cthulhu Teaches Typing',
     description: `TCthulhu Teaches Typing is a typing game for learning to type. Our goal was to target the keyboard enthusiast market with a minimal, yet customizable application to track and compare typing statistics. We also wanted to create a fun and engaging typing game that would help users learn to type faster and more accurately.`,
     imgSrc: '/static/images/projectCards/CTT.jpg',
