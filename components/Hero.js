9311630b28f09610b81d86af9dbc5d46a9a693b6
Curtis Warcup <88569957+Cwarcup@users.noreply.github.com>
2022-12-10 09:17:40 -0800

    Hero section icons

--- components/Hero.js
+++ components/Hero.js
@@ -1,31 +1,90 @@
-import siteMetadata from '@/data/siteMetadata'
 import Link from 'next/link'
-import PageTitle from './PageTitle'
+import { useState } from 'react'
+import { IoLogoGithub, IoLogoLinkedin, IoMail, IoCall } from 'react-icons/io5'
+import Notification from './Notification'
+
+function SocialLink({ icon: Icon, ...props }) {
+  return (
+    <Link className="-m-1 p-1 " {...props}>
+      <Icon className="h-6 w-6 cursor-pointer fill-gray-500 transition hover:fill-gray-200" />
+    </Link>
+  )
+}
+
+function CopyToClipboard({ icon: Icon, text, ...props }) {
+  const [show, setShow] = useState(false)
+
+  const handleClick = () => {
+    navigator.clipboard.writeText(text.contact)
+    setShow(!show)
+
+    setTimeout(() => {
+      setShow(false)
+    }, 3000)
+  }
+
+  return (
+    <div className="-m-1 p-1 " {...props}>
+      <Icon
+        className="h-6 w-6 cursor-pointer fill-gray-500 transition hover:fill-gray-200"
+        onClick={handleClick}
+      />
+      <Notification show={show} setShow={setShow} text={text} />
+    </div>
+  )
+}
 
 export default function Hero() {
   return (
-    <div className="flex w-full flex-col">
-      <div className="space-y-2 pb-4 text-center md:space-y-5 md:text-left">
-        <PageTitle>
-          Hi, I'm Curtis Warcup{' '}
-          <span role="img" aria-label="waving hand" className="wave">
-            👋
-          </span>
-        </PageTitle>
-        <p className=" prose max-w-none pb-4 text-lg leading-7 text-gray-400">
-          Technology enthusiast experienced in consumer electronics industry. I believe the optimal
-          code is achieved when the user and development experience is frictionless and intuitive.{' '}
-          <Link href={`mailto:${siteMetadata.email}`}>
-            <a
-              className="font-medium leading-6 "
-              aria-label={`Email to ${siteMetadata.email}`}
-              title={`Email to ${siteMetadata.email}`}
-            >
-              Get in touch &rarr;
-            </a>
-          </Link>
-        </p>
+    <div className="mb-5 max-w-2xl">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-800 dark:text-zinc-100 sm:text-5xl">
+        Web developer, tech enthusiast, and fitness junkie
+      </h1>
+      <p className="mt-6 text-base text-gray-600 dark:text-gray-400">
+        I'm Curtis, a web developer based in Vancouver BC. As a Lead at Apple, I keep the store
+        running smoothly on a daily basis. I've coordinated product launches, repair programs, and
+        store openings. I enjoy working with a team to solve problems and build solutions.
+      </p>
+      <div className="mt-6 flex gap-6">
+        <SocialLink
+          href="https://github.com/Cwarcup"
+          aria-label="Check out my Github"
+          icon={IoLogoGithub}
+        />
+        <SocialLink
+          href="https://www.linkedin.com/in/curtiswarcup/"
+          aria-label="Connect with me on LinkedIn"
+          icon={IoLogoLinkedin}
+        />
+        <CopyToClipboard
+          text={{ contact: 'curtis.gwarcup@gmail.com', type: 'Email' }}
+          aria-label="Send me an email"
+          icon={IoMail}
+        />
+        <CopyToClipboard
+          text={{ contact: '+1 (604) 374-4652', type: 'Phone number' }}
+          aria-label="Give me a call"
+          icon={IoCall}
+        />
       </div>
     </div>
   )
 }
+// <div className="flex flex-col w-full">
+//   <div className="pb-4 space-y-2 text-center md:space-y-5 md:text-left">
+//     <PageTitle>Web Developer, Tech Enthusiast, and Fitness Junkie</PageTitle>
+//     <p className="pb-4 text-lg leading-7 prose text-gray-400 max-w-none">
+//       Technology enthusiast experienced in consumer electronics industry. I believe the optimal
+//       code is achieved when the user and development experience is frictionless and intuitive.{' '}
+//       <Link href={`mailto:${siteMetadata.email}`}>
+//         <a
+//           className="font-medium leading-6 "
+//           aria-label={`Email to ${siteMetadata.email}`}
+//           title={`Email to ${siteMetadata.email}`}
+//         >
+//           Get in touch &rarr;
+//         </a>
+//       </Link>
+//     </p>
+//   </div>
+// </div>
---
+++ components/Notification.js
@@ -0,0 +1,63 @@
+import { Fragment, useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { IoCheckmarkCircleOutline, IoClose } from 'react-icons/io5'
+
+export default function Notification({ show, setShow, text: { contact, type } }) {
+  return (
+    <>
+      {/* Global notification live region, render this permanently at the end of the document */}
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 z-50 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-4">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <IoCheckmarkCircleOutline
+                      className="h-6 w-6 text-green-400"
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <div className="ml-3 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">{type} copied!</p>
+                    <p className="mt-1 text-sm text-gray-500">
+                      {contact} has been copied to your clipboard.
+                    </p>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      type="button"
+                      className="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                      <span className="sr-only">Close</span>
+                      <IoClose
+                        className="h-5 w-5"
+                        aria-hidden="true"
+                        onClick={() => {
+                          setShow(false)
+                        }}
+                      />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition>
+        </div>
+      </div>
+    </>
+  )
+}
